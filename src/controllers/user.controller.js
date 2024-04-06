a75217285d2e7855e159834505d9f89accec3b71
max925001 <shivam969684@gmail.com>
2024-04-06 03:08:58 -0700

    update user details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -291,4 +291,145 @@ try {
 
 
 
-export {registerUser ,loginUser ,logoutUser ,refreshAccessToken}
+
+const changeCurrentPassword = asyncHandler(async(req,res)=>{
+
+
+  const {oldPssword , newPassword} =req.body
+
+//agar user password change kar rha hai to iska mtlb hai vo login hai to hum authmiddleware ka use karke id le sakte haii
+
+const user =User.findById(req.user?._id)
+ const isPasswordCorrect =await user.isPsswordCorrect(oldPssword)
+ if(!isPasswordCorrect){
+
+  throw new ApiError(400,"Invalid old password")
+
+
+ }
+
+ user.password =newPassword
+ await user.save({validateBeforeSave:false})
+
+ return res
+ .status(200)
+ .json(new ApiResponse(200,{} ,"Password change successfully"))
+
+
+  
+})
+
+const getCurrentUser = asyncHandler(async(req,res)=>{
+
+return res.status(200)
+.json(200,req.user,"current user fetched successfully")
+
+
+})
+
+const updateAccountDetails = asyncHandler(async(req,res) =>{
+
+const {fullName ,email} = req.body
+
+if(!fullName || !email){
+  throw new ApiError(400 ,"All fields are required")
+
+
+}
+
+const user = User.findByIdAndUpdate(req.user?._id,
+{
+$set:{ // iski help new value set hoga
+
+  fullName,
+  email
+}
+
+},
+{new:true}
+
+).select("-password")
+
+
+return res
+.status(200)
+.json(new ApiResponse(200 ,user ,"Account details updated successfully"))
+
+})
+
+const updateUserAvatar = asyncHandler(async(req,res)=>{
+
+  const avatarLocalPath = req.file?.path
+
+  if(!avatarLocalPath){
+    throw new ApiError(400 ,"Avatar file missing")
+
+
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+  if(!avatar.url){
+    throw new ApiError(400,"Error while uploading on avatar")
+
+  }
+
+  const user =await User.findByIdAndUpdate(req.user?._id
+  ,
+{
+  $set:{
+    avatar:avatar.url // yha url ish liye likha hai kyu ki humko bas url dena hai avatar me pura object nhi barna hai
+
+  }
+},
+{new: true})
+
+return res.status(200)
+.json(
+  new ApiResponse(200 ,user,"avatar image update successfully")
+)
+
+})
+
+const updateUserCoverImage = asyncHandler(async(req,res)=>{
+
+  const coverImageLocalPath = req.file?.path
+
+  if(!coverImageLocalPath){
+    throw new ApiError(400 ,"coverImage file missing")
+
+
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if(!coverImage.url){
+    throw new ApiError(400,"Error while uploading on avatar")
+
+  }
+
+  const user = await User.findByIdAndUpdate(req.user?._id
+  ,
+{
+  $set:{
+    avatar:avatar.url // yha url ish liye likha hai kyu ki humko bas url dena hai avatar me pura object nhi barna hai
+
+  }
+},
+{new: true}).select("-password")
+
+
+return res.status(200)
+.json(
+  new ApiResponse(200 ,user,"CoverImage image update successfully")
+)
+})
+
+
+export {registerUser ,loginUser ,logoutUser ,refreshAccessToken
+,getCurrentUser,
+changeCurrentPassword
+,updateAccountDetails,
+updateUserAvatar,
+updateUserCoverImage
+}
