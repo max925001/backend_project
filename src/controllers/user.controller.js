3a4eb8af0fba3d25f670d5c618f4474248afa95a
max925001 <shivam969684@gmail.com>
2024-04-10 09:16:59 -0700

    learning concept of aggregation pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -322,7 +322,7 @@ const user =User.findById(req.user?._id)
 const getCurrentUser = asyncHandler(async(req,res)=>{
 
 return res.status(200)
-.json(200,req.user,"current user fetched successfully")
+.json(new ApiResponse(200,req.user,"current user fetched successfully"))
 
 
 })
@@ -337,7 +337,7 @@ if(!fullName || !email){
 
 }
 
-const user = User.findByIdAndUpdate(req.user?._id,
+const user = await User.findByIdAndUpdate(req.user?._id,
 {
 $set:{ // iski help new value set hoga
 
@@ -426,10 +426,122 @@ return res.status(200)
 })
 
 
+
+
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+  
+const {username } =req.params
+if(!username?.trim()){
+  throw new ApiError(400 ,"username is required")}
+
+
+
+//User.find({username})
+
+
+//aggreation ek array leta hai
+  const channel =await User.aggregate([
+  {
+    $match:{
+      username:username?.toLowerCase()
+    }
+  },
+  {
+    $lookup:{
+      from:"subscriptions",
+      localField:"_id",
+      foreignField:"channel",
+      as:"subscribers"
+    }
+  },
+  {
+
+   
+    $lookup:{
+      from:"subscriptions",
+      localField:"_id",
+      foreignField:"subscriber",
+      as:"subscribedTo"
+    }
+
+
+
+  },
+
+
+  // additional models
+{
+  
+
+  $addFields:{
+    
+    subscribersCount:{$size:"$subscribers"},
+    channelSubscribedToCount:{$size:"$subscribedTo"},
+
+    isSubscribed:{
+      $cond:{
+        if:{
+          $in:[req.user._id, "$subscribers.subscriber"]
+        },
+        then:true,
+        else:false
+  
+      }
+    },
+    
+
+  },
+ 
+  
+
+},
+{
+
+
+  $project:{
+fullName:1,
+username:1,
+subscribersCount:1,
+channelSubscribedToCount:1,
+isSubscribed:1, 
+avatar:1,
+coverImage:1,
+email:1,
+
+
+
+
+
+
+  }
+
+
+
+
+}
+
+
+
+])
+
+if(!channel?.length){
+  throw new ApiError(404 ,"No channel found")
+}
+return res
+.status(200)
+.json(
+  new ApiResponse(200 ,channel[0],"Channel fetched successfully")
+)
+
+})
+
+
+
 export {registerUser ,loginUser ,logoutUser ,refreshAccessToken
 ,getCurrentUser,
 changeCurrentPassword
 ,updateAccountDetails,
 updateUserAvatar,
-updateUserCoverImage
+updateUserCoverImage,
+getUserChannelProfile
 }
